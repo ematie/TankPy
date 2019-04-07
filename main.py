3185d76dd71dc2432cd988eca918af08e8798aae
ematie <40446994+ematie@users.noreply.github.com>
2019-04-07 22:13:15 +0200

    another aproch
    another aproch, more classic style.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 # Acceso a la libreria 
 import pygame
+import os
 
 
 # Constantes
@@ -12,6 +13,9 @@ SCREEN_HEIGHT = 800 # Altura de la pantalla
 WHITE_COLOR = (255, 255, 255)# Color blanco
 BLACK_COLOR = (0,   0,   0)  # Color negro
 SCREEN_TITLE = "pyTANK 0.1"  # Nombre del juego
+GAME_FOLDER = os.path.dirname(__file__)
+SPRITES_FOLDER = os.path.join(GAME_FOLDER, "images")
+
 clock = pygame.time.Clock()
 pygame.font.init()
 font = pygame.font.SysFont("Comicsans", 75)
@@ -24,69 +28,67 @@ class Game():
         self.title = title
         self.width = width
         self.height = height
-
+        self.all_sprites = pygame.sprite.Group()
         self.game_screen = pygame.display.set_mode((width, height))
-        self.game_screen.fill(WHITE_COLOR)
+        sherman = Tank(400, 300)
+        self.all_sprites.add(sherman)
+        self.game_screen.fill(BLACK_COLOR)
         pygame.display.set_caption(title)
 
     def run_game(self):
         is_game_over = False
         direction = 0
-
-        sherman = Tank("images/body.png", 200, 300, 142, 124)
         
         while not is_game_over:
             for event in pygame.event.get():
+                
                 if event.type == pygame.QUIT:
                     is_game_over = True
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
-                        direction = 1
-                    elif event.key == pygame.K_s:
-                        direction = -1
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_w or event.key == pygame.K_s:
-                        direction = 0
-
+					
                 print (event)
 
-            self.game_screen.fill(WHITE_COLOR)    
-            sherman.move(direction)
-            sherman.draw(self.game_screen)
+            self.game_screen.fill(BLACK_COLOR)
+            self.all_sprites.update()
+            self.all_sprites.draw(self.game_screen)
             
-            
-            pygame.display.update()
             clock.tick(self.TICK)
+            pygame.display.flip()        
 
-class GameObjects:
-    def __init__(self, image_path, x, y, width, hight):
-        object_image = pygame.image.load(image_path)
-        self.sprite = pygame.transform.scale(object_image, (width, hight))
-        self.x_pos = x
-        self.y_pos = y
-        self.width = width
-        self.height = hight
 
-    def draw(self, background):
-        background.blit(self.sprite, (self.x_pos, self.y_pos))
         
 
-class Tank(GameObjects):
+class Tank(pygame.sprite.Sprite):
+        
+    def __init__(self, x, y, angle = 0):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(SPRITES_FOLDER, "body.png")).convert()
+        self.image = pygame.transform.rotate(self.image, 270)
+        self.image.set_colorkey(BLACK_COLOR)
+        self.rect = self.image.get_rect()
+        self.rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        self.speedy = 2
+        self.speedr = 2
+        
+    def update(self):
+        keystate = pygame.key.get_pressed()
+        
+        if keystate[pygame.K_w] and self.rect.top > 0:
+            self.rect.top -= self.speedy
+        if keystate[pygame.K_s] and self.rect.bottom < SCREEN_HEIGHT:
+            self.rect.top += self.speedy
 
-    SPEED = 10
-    def __init__(self, image_path, x, y, width, hight):
-        super().__init__(image_path, x, y, width, hight)
-
-    def move(self, direction):
-        if direction > 0:
-            self.y_pos -= self.SPEED
-        elif direction < 0:
-            self.y_pos += self.SPEED
         
             
+    def draw(self):
+        pass
+
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(slef):
+#         pygame.sprite.Sprite.__init__(self)
+        pass
         
     
-
 pygame.init()
 
 pytank = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
